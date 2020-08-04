e2009be689b8956cd45ea4cee402876fc21fa5ad
Vinicius Vicentini <viniciusvicentini@live.com>
2020-08-04 18:59:32 -0300

    componente de categorias

---
+++ components/Tags.vue
@@ -0,0 +1,34 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item">
+      <n-link :to="{ name: 'buscar', query: { filtro: item } }" prefetch>
+        {{ item }}
+
+        <span class="divider" />
+      </n-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+}
+</script>
+
+<style lang="scss" scoped>
+li {
+  display: inline-block;
+
+  &:not(:last-child) {
+    .divider {
+      &::after {
+        content: '·';
+        font-weight: 900;
+        margin-right: 7px;
+        margin-left: 5px;
+      }
+    }
+  }
+}
+</style>
