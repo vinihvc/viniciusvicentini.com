a28f0646052b4b77466762bf9d8f348c6e6b1fd9
Vinicius Vicentini <viniciusvicentini@live.com>
2021-11-18 00:00:07 -0300

    feat: new props to Link component

--- src/components/primitives/Link.tsx
+++ src/components/primitives/Link.tsx
@@ -1,6 +1,8 @@
-import { styled } from 'stitches.config'
+import { AnchorHTMLAttributes, forwardRef } from 'react'
 
-export const Link = styled('a', {
+import { styled, CSS, VariantProps } from 'stitches.config'
+
+export const LinkStyled = styled('a', {
   textDecoration: 'none',
   color: '$white',
   cursor: 'pointer',
@@ -26,6 +28,40 @@ export const Link = styled('a', {
       },
     },
   },
+})
+
+type LinkProps = {
+  /**
+   *  If `true`, the link will open in new tab
+   */
+  isExternal?: boolean
+  /**
+   *  Add a decoration to the link
+   */
+  decorated?: VariantProps<typeof LinkStyled>['decorated']
+  /**
+   * The content of the link
+   */
+  css?: CSS
+  /**
+   * The content of the link
+   */
+  children?: React.ReactNode
+} & AnchorHTMLAttributes<HTMLAnchorElement>
+
+const Link = forwardRef<HTMLAnchorElement, LinkProps>((props, ref) => {
+  const { isExternal, children, ...rest } = props
+
+  return (
+    <LinkStyled
+      ref={ref}
+      rel={isExternal ? 'noopener noreferrer' : undefined}
+      target={isExternal ? '_blank' : undefined}
+      {...rest}
+    >
+      {children}
+    </LinkStyled>
+  )
 })
 
 export default Link
