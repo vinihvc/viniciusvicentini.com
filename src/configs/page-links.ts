8edfcf92e5a3ebc595be5b42d0931012bf825888
Vinicius Vicentini <viniciusvicentini@live.com>
2021-11-18 00:06:44 -0300

    feat: organizing repetitive links in configs

---
+++ src/configs/page-links.ts
@@ -0,0 +1,20 @@
+const pageLinks = [
+  {
+    href: '/',
+    label: 'Home',
+  },
+  {
+    href: '/about',
+    label: 'About',
+  },
+  {
+    href: '/projects',
+    label: 'Projects',
+  },
+  {
+    href: '/blog',
+    label: 'Blog',
+  },
+]
+
+export default pageLinks
---
+++ src/configs/social-links.ts
@@ -0,0 +1,26 @@
+import { BsGithub, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs'
+
+const socialLinks = [
+  {
+    title: 'Github',
+    icon: BsGithub,
+    link: 'https://github.com/viniciushvc',
+  },
+  {
+    title: 'Twitter',
+    icon: BsTwitter,
+    link: 'https://twitter.com/viniciushvc',
+  },
+  {
+    title: 'Linkedin',
+    icon: BsLinkedin,
+    link: 'https://www.linkedin.com/in/viniciushvc',
+  },
+  {
+    title: 'Instagram',
+    icon: BsInstagram,
+    link: 'https://www.instagram.com/viniciushvc',
+  },
+]
+
+export default socialLinks
