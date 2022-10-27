670156e5fb312fb007f887f753b4d47c81dc204f
Vinicius Vicentini <viniciusvicentini@live.com>
2022-10-27 11:55:03 -0300

    update packages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-const withPWA = require('next-pwa', {
+const withPWA = require('next-pwa')({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 })
