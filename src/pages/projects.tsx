d8a7bec34b5dbac1730b1b6a5e0a2e9782958887
Vinicius Vicentini <viniciusvicentini@live.com>
2023-01-02 10:23:06 -0300

    fix stack

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -21,43 +21,44 @@ const ProjectsPage = () => {
           projects
         </Hero>
 
-        <Stack></Stack>
-        <Text as="h3" size="2xl" weight="bold">
-          Featured Projects
-        </Text>
+        <Stack direction="column">
+          <Text as="h3" size="2xl" weight="bold">
+            Featured Projects
+          </Text>
 
-        <Grid
-          css={{
-            columns: 'repeat(1, 1fr)',
-            gap: 80,
-            '@md': {
-              columns: 'repeat(3, 1fr)',
-              gap: '$5',
-            },
-          }}
-        >
-          {projectLinks?.map((item) => (
-            <ProjectCard key={item.title} {...item} />
-          ))}
-        </Grid>
-
-        <Text as="h4" weight="normal" css={{ py: '$10' }}>
-          {'Check out all my projects on '}
-
-          <Link
-            href="https://github.com/vinihvc?tab=repositories"
-            decorated
-            isExternal
+          <Grid
             css={{
-              color: '$primary',
-              '&:hover': {
-                color: '$primary',
+              columns: 'repeat(1, 1fr)',
+              gap: 80,
+              '@md': {
+                columns: 'repeat(3, 1fr)',
+                gap: '$5',
               },
             }}
           >
-            GitHub
-          </Link>
-        </Text>
+            {projectLinks?.map((item) => (
+              <ProjectCard key={item.title} {...item} />
+            ))}
+          </Grid>
+
+          <Text as="h4" weight="normal" css={{ py: '$10' }}>
+            {'Check out all my projects on '}
+
+            <Link
+              href="https://github.com/vinihvc?tab=repositories"
+              decorated
+              isExternal
+              css={{
+                color: '$primary',
+                '&:hover': {
+                  color: '$primary',
+                },
+              }}
+            >
+              GitHub
+            </Link>
+          </Text>
+        </Stack>
       </Container>
     </>
   )
