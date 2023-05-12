de4a451865aeae5bae9cea50180755f38236da8d
Vinicius Vicentini <viniciusvicentini@live.com>
2023-05-12 20:13:11 +0100

    update SEO

--- constants/seo.ts
+++ constants/seo.ts
@@ -1,6 +1,7 @@
 export const SEO = {
   title: 'Vinicius Vicentini',
-  description: 'Start your project with Next and all modern technologies.',
+  description:
+    'Front-end engineer who loves UX, Performance and Design. Welcome to my personal blog',
   keywords: ['Frontend', 'Developer', 'React', 'Tailwind', 'Next'],
   thumb: '/images/thumb.png',
   url: 'https://www.viniciusvicentini.com',
