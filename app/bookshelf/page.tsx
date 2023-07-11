d004bd2622e7e1667e3e135e6c51d8e6a7d90dc9
Vinicius Vicentini <vini@cyberbrokers.io>
2023-07-11 20:17:36 +0100

    add google books api

--- app/bookshelf/page.tsx
+++ app/bookshelf/page.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
 import { Metadata } from 'next'
+import Image from 'next/image'
 
 import { Title } from '@/components/title'
 
@@ -9,94 +10,61 @@ export const metadata: Metadata = {
   title: PAGE_NAME,
 }
 
-const ReadingPage = () => {
+const getData = async () => {
+  const books = [
+    'zDsqDwAAQBAJ',
+    'F4RNEAAAQBAJ',
+    'TXXTDwAAQBAJ',
+    'M94NEAAAQBAJ',
+    '2CEJnm7PRtIC',
+    'jEBdAwAAQBAJ',
+    '1yYuB7N0HPIC',
+    'JPwkDwAAQBAJ',
+  ]
+
+  const res = await Promise.all(
+    books.map(async (book) => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/volumes/${book}`,
+      )
+      return res
+    }),
+  )
+
+  const data = await Promise.all(res.map((r) => r.json()))
+
+  return data
+}
+
+const ReadingPage = async () => {
+  const books = await getData()
+
   return (
     <div className="container max-w-3xl">
       <Title className="from-cyan-300 to-fuchsia-700">{PAGE_NAME}</Title>
 
       <div className="space-y-5">
-        {[...Array(3)].map((_, i) => (
-          <div className="space-y-4">
-            <h2>
-              {i === 0 && 'Reading'}
-              {i === 1 && 'Readed'}
-              {i === 2 && 'Want to read'}
-            </h2>
+        <div className="grid grid-cols-4 items-start gap-2">
+          {books?.map((book, i) => (
+            <article key={i} className="relative overflow-hidden rounded">
+              <div className="aspect-[9/13]">
+                <Image
+                  src={book.volumeInfo.imageLinks.thumbnail}
+                  alt={book.volumeInfo?.title}
+                  fill
+                />
+              </div>
 
-            <div className="grid grid-cols-1 gap-5 md:grid-cols-5">
-              {[...Array(4)].map((_, i) => (
-                <article key={i}>
-                  <div>
-                    <img
-                      src="https://render.fineartamerica.com/images/rendered/default/print/6/8/break/images/artworkimages/medium/1/no844-my-to-kill-a-mockingbird-minimal-movie-poster-chungkong-art.jpg"
-                      alt=""
-                      className="aspect-[9/16] h-36 w-full rounded object-cover text-red-500 shadow-md"
-                    />
+              <div className="absolute inset-x-0 bottom-0">
+                <div className="flex h-10 items-center bg-black/50 px-2 backdrop-blur-sm">
+                  <div className="truncate text-sm font-bold">
+                    {book.volumeInfo?.title}
                   </div>
-
-                  <div className="mt-2">
-                    <div className="font-bold">Article {i + 1}</div>
-
-                    <div className="text-sm text-gray-500">Fernando Pessoa</div>
-
-                    <div className="flex items-center">
-                      <svg
-                        aria-hidden="true"
-                        className="h-3 w-3 text-yellow-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>First star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                      </svg>
-                      <svg
-                        aria-hidden="true"
-                        className="h-3 w-3 text-yellow-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Second star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                      </svg>
-                      <svg
-                        aria-hidden="true"
-                        className="h-3 w-3 text-yellow-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Third star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                      </svg>
-                      <svg
-                        aria-hidden="true"
-                        className="h-3 w-3 text-yellow-400"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Fourth star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                      </svg>
-                      <svg
-                        aria-hidden="true"
-                        className="h-3 w-3 text-gray-300 dark:text-gray-500"
-                        fill="currentColor"
-                        viewBox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <title>Fifth star</title>
-                        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
-                      </svg>
-                    </div>
-                  </div>
-                </article>
-              ))}
-            </div>
-          </div>
-        ))}
+                </div>
+              </div>
+            </article>
+          ))}
+        </div>
       </div>
     </div>
   )
