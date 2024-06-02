8b186a71ff370ea5679cb345e1eeebc41e5d08ce
Vinicius Vicentini <viniciusvicentini@live.com>
2024-06-02 04:06:55 -0300

    update craft color

--- app/craft/page.tsx
+++ app/craft/page.tsx
@@ -4,7 +4,7 @@ const CraftPage = () => {
 	return (
 		<section className="container selection:bg-amber-500 selection:text-black">
 			<div className="space-y-1">
-				<Title className="from-amber-500 to-red-500">Craft</Title>
+				<Title className="from-rose-500 to-purple-500">Craft</Title>
 
 				<h2 className="text-muted text-lg">
 					Experiments and projects I've been working on. 🛠️
