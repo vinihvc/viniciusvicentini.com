560e853035bc141a6ae837a470b51e4fdb265a0b
Vinicius Vicentini <viniciusvicentini@live.com>
2024-06-05 00:56:59 -0300

    remove initial animation

--- app/page.tsx
+++ app/page.tsx
@@ -8,33 +8,31 @@ import { Button } from '@/components/ui/button'
 const HomePage = () => {
 	return (
 		<div className="flex-1 container selection:bg-blue-500 flex flex-col justify-center">
-			<div className="animate-in fade-in-0 slide-in-from-bottom-10">
-				<div className="space-y-2">
-					<h1 className="text-3xl font-bold sm:text-4xl">Vinicius Vicentini</h1>
+			<div className="space-y-2">
+				<h1 className="text-3xl font-bold sm:text-4xl">Vinicius Vicentini</h1>
 
-					<h2 className="text-muted text-lg">
-						Software engineer who loves UX, Performance and Design.
-					</h2>
-				</div>
+				<h2 className="text-muted text-lg">
+					Software engineer who loves UX, Performance and Design.
+				</h2>
+			</div>
 
-				<div className="flex gap-2 sm:gap-5 mt-5">
-					{SOCIAL_LINKS.map((link) => (
-						<Button
-							key={link.link}
-							className={cn('p-4 size-auto', link.className)}
-							asChild
-						>
-							<Link href={link.link} isExternal>
-								{React.createElement(link.icon, {
-									className: 'size-6',
-									'aria-hidden': true,
-								})}
+			<div className="flex gap-2 sm:gap-5 mt-5">
+				{SOCIAL_LINKS.map((link) => (
+					<Button
+						key={link.link}
+						className={cn('p-4 size-auto', link.className)}
+						asChild
+					>
+						<Link href={link.link} isExternal>
+							{React.createElement(link.icon, {
+								className: 'size-6',
+								'aria-hidden': true,
+							})}
 
-								<span className="sr-only">{`Visit my ${link.title}`}</span>
-							</Link>
-						</Button>
-					))}
-				</div>
+							<span className="sr-only">{`Visit my ${link.title}`}</span>
+						</Link>
+					</Button>
+				))}
 			</div>
 		</div>
 	)
